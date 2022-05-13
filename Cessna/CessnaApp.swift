569caf116ebfe42b88a3bbea6e41e17d32469caf
Sergei Kriukov <admtransmedia@icloud.com>
2022-05-13 16:04:24 +0600

    Initial Commit

---
+++ Cessna/CessnaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CessnaApp.swift
+//  Cessna
+//
+//  Created by Sergei Kriukov on 13.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CessnaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
